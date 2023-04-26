b9925216bb90beedc8e9f31013b74656e5b73b2a
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-04-26 20:54:38 -0300

    adicionando display na view

--- src/View/Calculator.tsx
+++ src/View/Calculator.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { CalculatorContainer, Container } from "./styles";
 
 import Keyboard from "../Components/Keyboard";
+import Display from "../Components/Display";
 
 const Calculator = () => {
   return (
     <Container>
       <CalculatorContainer>
+        <Display />
         <Keyboard />
       </CalculatorContainer>
     </Container>
