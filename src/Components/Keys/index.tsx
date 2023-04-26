8bb53b2bdc4ea8a81c53a72c963b3f82081734c4
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-04-26 20:54:01 -0300

    criando componente key

--- src/Components/Keys/index.tsx
+++ src/Components/Keys/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Keys = ({ keyName }: any) => {
-  return <Container>{keyName}</Container>;
+const Keys = ({ keyName, colored, onClick }: any) => {
+  return (
+    <Container onClick={onClick} colored={colored}>
+      {keyName}
+    </Container>
+  );
 };
 
 export default Keys;
