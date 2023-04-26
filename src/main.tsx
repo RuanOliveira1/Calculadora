702ac239cf19a037cd1e259ec675402d80f97ba1
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-04-26 20:53:12 -0300

    adicionando provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Calculator from "./View/Calculator.tsx";
+import { CalculatorProvider } from "./Context/Calculator.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Calculator />
+    <CalculatorProvider>
+      <Calculator />
+    </CalculatorProvider>
   </React.StrictMode>
 );
