2d8bb8689a662a96581b6f451e47c0168421f0f1
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-04-26 19:33:10 -0300

    Enviando arquivos

---
+++ src/View/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Calculator = () => {
+  return (
+    <Container>
+      <CalculatorContainer></CalculatorContainer>
+    </Container>
+  );
+};
+
+export default Calculator;
---
+++ src/View/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  width: 100%;
+  height: 100%;
+
+  background: #807ece;
+`;
+
+export const CalculatorContainer = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  width: 100%;
+  height: 100%;
+
+  background: #807ece;
+`;
